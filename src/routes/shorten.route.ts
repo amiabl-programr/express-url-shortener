9db2dc7d5bb4e3571b728de7872c76cf3726f99e
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-18 18:26:02 +0100

    feat: add shorten.route.ts for URL routing functionality

---
+++ src/routes/shorten.route.ts
@@ -0,0 +1,8 @@
+import {createShortURLController, getShortURLController} from '../controllers/shorten.controller';
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/shorten', createShortURLController);
+router.get('/shorten/:hashValue', getShortURLController);
+export default router;
