4cf90da0df1d598c2c7e159b0dcdc5f49757034a
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-18 18:31:14 +0100

    feat: add app.ts for server setup and routing configuration

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Express } from 'express';
+import shortenRoute from './routes/shorten.route';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: Express = express();
+
+
+app.use(express.json());
+app.use('/api', shortenRoute);
+
+app.use((req, res, next) => {
+    res.status(404).json({ status: 'error', message: 'Endpoint not found' });
+    next();
+});
+
+export default app;
+
+
