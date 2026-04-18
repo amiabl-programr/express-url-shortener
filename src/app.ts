bfd0a26e916d2c185b0ed2f1b82907b9c5a0f708
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-18 18:35:19 +0100

    fix: update parameter name in 404 error handler for consistency

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Express = express();
 app.use(express.json());
 app.use('/api', shortenRoute);
 
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
     res.status(404).json({ status: 'error', message: 'Endpoint not found' });
     next();
 });
