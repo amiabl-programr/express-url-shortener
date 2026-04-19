23d1cefdf6e56ba5ce3bb47bcc4ee05368fc98ee
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-19 16:17:11 +0100

    refactor: streamline short URL generation logic in createShortURL function

--- src/service/shorten.service.ts
+++ src/service/shorten.service.ts
@@ -3,7 +3,6 @@ import { uuid } from 'uuidv4';
 
 export async function createShortURL(originalUrl: string): Promise<string> {
   console.log('Original URL:', originalUrl);
-  let shortUrl;
   // check if originalUrl already exists in the database
   const existing = await client.query('SELECT short_url FROM urls WHERE long_url = $1', [
     originalUrl,
@@ -12,23 +11,17 @@ export async function createShortURL(originalUrl: string): Promise<string> {
     return existing.rows[0].short_url;
   }
   // insert new URL mapping into the database
-  if (process.env.NODE_ENV === 'production') {
-    // Use a more secure method for generating short URLs in production
-    shortUrl = `${process.env.SHORT_URL_BASE_PROD}/${uuid()}`;
-    await client.query('INSERT INTO urls (short_url, long_url) VALUES ($1, $2)', [
-      shortUrl,
-      originalUrl,
-    ]);
-    return shortUrl;
-  } else {
-    // Use a simple random string for development
-    shortUrl = `${process.env.SHORT_URL_BASE_DEV}/${uuid()}`;
-    await client.query('INSERT INTO urls (short_url, long_url) VALUES ($1, $2)', [
-      shortUrl,
-      originalUrl,
-    ]);
-    return shortUrl;
-  }
+  const baseUrl =
+    process.env.NODE_ENV === 'production'
+      ? process.env.SHORT_URL_BASE_PROD
+      : process.env.SHORT_URL_BASE_DEV;
+
+  const shortUrl = `${baseUrl}/${uuid()}`;
+  await client.query('INSERT INTO urls (short_url, long_url) VALUES ($1, $2)', [
+    shortUrl,
+    originalUrl,
+  ]);
+  return shortUrl;
 }
 
 export async function getShortURLfromDb(hashValue: string) {
