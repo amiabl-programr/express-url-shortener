ac79a4d8874565b9c2e8eaea3780c4a2fdb29450
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-05-02 17:43:50 +0100

    feat: configure base url to serve the statis files

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Express } from 'express';
 import shortenRoute from './routes/shorten.route.js';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 dotenv.config();
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api', shortenRoute);
 
 app.use((_req, res, next) => {
